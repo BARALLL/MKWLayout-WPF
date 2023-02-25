5e966df3f06721af1502ff802e58a28751a5372c
BARALLL <54141221+BARALLL@users.noreply.github.com>
2023-02-25 18:39:09 +0100

    Ajoutez des fichiers projet.

---
+++ MKWLayout-WPF/NNet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MKWLayout_WPF
+{
+    public class NNet
+    {
+        List<String> imgDBPath = new List<String>();
+
+        /// <summary>
+        /// Summary description for Class1
+        /// </summary>
+        public NNet()
+        {
+
+        }
+
+        public void LoadNNet()
+        {
+
+        }
+
+        public void SaveNNet()
+        {
+
+        }
+
+        public void TrainNNet()
+        {
+
+        }
+    }
+}
